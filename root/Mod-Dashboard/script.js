8e73ddc9b7cde768b5e280341c09a7e728b5c58c
NJ Berman <njberman16@hotmail.com>
2021-07-01 08:02:35 +0100

    add stuff

---
+++ root/Mod-Dashboard/script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // if (document.referrer != `${window.location.origin}/flappy-bird/index.html` || document.referrer == window.location.href) {
+  //   return window.location.href = 'https://youtube.com/search?search_query=Don\'t try to hack the system.';
+  // }
+
+  // const username = prompt('Please enter your username:');
+  // const password = prompt('Please enter your password:');
+	// // const url = 'https://flappy-bird-highscore.herokuapp.com';
+	// const url = 'http://localhost:3000';
+
+  // fetch(url + '/login-dashboard', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   },
+  //   body: JSON.stringify({ username, password }),
+  // })
+  //   .then((res) => {
+  //     if (res.status == 400) window.location.href = window.location.origin;
+  //     else return;
+  //   })
+  //   .catch(console.error);
+});

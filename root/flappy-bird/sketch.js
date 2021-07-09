c9be147185a45c87fcc1ae040734c19fd829a301
njberman <67945050+njberman@users.noreply.github.com>
2021-07-09 11:25:03 +0100

    Update sketch.js

--- root/flappy-bird/sketch.js
+++ root/flappy-bird/sketch.js
@@ -134,7 +134,7 @@ function preload() {
         light => loadImage(`assets/sprites/background-${light}.png`)
     );
 
-    birdsImg = ["pride"].map(
+    birdsImg = ["yellow"].map(
         color => ["midflap", "upflap", "downflap"].map(
             flap => loadImage(`assets/sprites/${color}bird-${flap}.png`)
         )

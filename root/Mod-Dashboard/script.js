b009afb02303ef651dfe9a919c9a50e42be33569
njberman <67945050+njberman@users.noreply.github.com>
2021-07-15 08:47:50 +0100

    Update script.js

--- root/Mod-Dashboard/script.js
+++ root/Mod-Dashboard/script.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify({ name: newUserNameInput.value, score: newUserScoreInput.value }),
+          body: JSON.stringify({ name: newUserNameInput.value, score: newUserScoreInput.value, createdBy: username }),
         })
           .then(() => getLeaderboard())
           .catch(console.error);

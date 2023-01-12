7eb29faf46984d013f4c7d3afdd6167a729fdbd0
njberman <67945050+njberman@users.noreply.github.com>
2023-01-12 20:20:44 +0100

    Change to render

--- root/flappy-bird/sketch.js
+++ root/flappy-bird/sketch.js
@@ -74,7 +74,7 @@ function limit(string = '', limit = 0) {
 }
 
 function scoreTings(type) {
-	const url = 'https://flappy-bird-server.vercel.app/';
+	const url = 'https://flappy-bird-server.onrender.com';
 // 	const url = 'http://localhost:3000';
 	if (type == 'GET') {
 		fetch(url)
